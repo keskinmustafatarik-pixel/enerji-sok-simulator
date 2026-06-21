b74d6cdd3db0e0e065dd53a96fae3612a5354b67
keskinmustafatarik-pixel <keskinmustafatarik@gmail.com>
2026-06-21 17:26:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ st.set_page_config(page_title="Enerji Şok Simülatörü", layout="centered")
 
 df = pd.read_excel("DATA.xls")
 
+# Sütun adlarını temizle
+df.columns = df.columns.str.replace("\n", "", regex=False).str.strip()
+
+# Sektör isimlerini temizle
+df["Sektor"] = df["Sektor"].astype(str).str.replace("\n", "", regex=False).str.strip()
+
 st.title("Enerji Şok Simülatörü")
 
 st.write(
@@ -12,17 +18,29 @@ st.write(
     "seçilen sektörün maliyetine yaklaşık etkisini hesaplar."
 )
 
-sektor = st.selectbox("Sektör seç", df["Sektör"].unique())
+sektor = st.selectbox("Sektör seç", df["Sektor"].unique())
 
 enerji = st.selectbox(
     "Enerji ürünü seç",
-    ["Kömür", "Petrol", "Doğalgaz", "Elektrik", "Isı"]
+    [
+        "Komur_urunleri",
+        "Petrol_urunleri",
+        "Dogal_Gaz",
+        "Elektrik",
+        "Isı",
+        "Diger_urunler"
+    ]
 )
 
 degisim = st.number_input("Fiyat değişimi (%)", value=20.0)
 
-satir = df[df["Sektör"] == sektor].iloc[0]
+satir = df[df["Sektor"] == sektor].iloc[0]
 katsayi = satir[enerji]
+
+# Eğer Excel'de değerler %16 gibi girildiyse pandas bunu 0.16 okuyabilir.
+# Eğer 16 olarak okursa 100'e bölüyoruz.
+if katsayi > 1:
+    katsayi = katsayi / 100
 
 sonuc = katsayi * degisim
 
